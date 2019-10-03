cc004b3fdb78a37c4c2b132186c0572deb7794a5
Nour El-Sharkawi <Nour@Nours-MacBook-Pro-703.local>
2019-10-03 16:36:51 +0300

    Before Last

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Page(models.Model):
+	"""docstring for Page"""
+	title = models.CharField(max_length=120)
+	content = models.TextField()
+	last_update = models.DateTimeField(auto_now_add=True)
+
+	# def __init__(self, arg):
+	# 	super( Page, self).__init__()
+	# 	self.arg = arg
+		
